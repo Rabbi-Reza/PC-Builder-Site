94479193c4bc48ff7208321b1daa6aef6ea910dd
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2023-07-28 18:25:17 +0600

    Add Google and Github login

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Layout/Navbar";
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Navbar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,57 @@
+import styles from "@/styles/Login.module.css";
+import { GithubOutlined, GoogleOutlined } from "@ant-design/icons";
+import { signIn } from "next-auth/react";
+import Head from "next/head";
+import { useForm } from "react-hook-form";
+
+const LoginPage = () => {
+//   console.log(user);
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    // console.log(data);
+    createUserWithEmailAndPassword(data.email, data.password);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Next Login</title>
+      </Head>
+      <div className={styles.form}>
+        <h3>LOGIN</h3>
+        <div className={styles.social_icons}>
+          <GoogleOutlined
+            onClick={() =>
+              signIn("google", {
+                callbackUrl: "/",
+              })
+            }
+          />
+          <GithubOutlined
+            onClick={() =>
+              signIn("github", {
+                callbackUrl: "/",
+              })
+            }
+          />
+        </div>
+        {/* <hr /> */}
+        {/* <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="">Your Email</label>
+          <input {...register("email", { required: true })} type="email" />
+          <label>Your Password</label>
+          <input
+            {...register("password", { required: true })}
+            htmlFor=""
+            type="password"
+          />
+          <button type="submit">Login</button>
+        </form> */}
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
